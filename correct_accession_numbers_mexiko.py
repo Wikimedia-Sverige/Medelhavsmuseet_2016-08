7d34274ebdf558893c8f396d1a24c0fac05599d9
mattiasostmar <mattiasostmar@gmail.com>
2017-01-09 13:19:18 +0100

    add count alterations, improve write logic

--- correct_accession_numbers_mexiko.py
+++ correct_accession_numbers_mexiko.py
@@ -4,6 +4,7 @@ import pywikibot
 import time
 
 log = open("./correct_accession_numbers.log","w")
+no_alterations = 0
 
 site = pywikibot.Site(fam="commons")
 
@@ -52,11 +53,8 @@ mexiko_arkiv = pd.read_excel("excel-export.xls", sheetname="Mexiko-Arkiv")
 
 for index, fotonr in enumerate(old_json.keys()):
     fotonr_plus_ext = fotonr + ".tif"
-
-    print("File no {} of {}".format(index, len(old_json.keys())))
     json_infotext = old_json[fotonr]["info"]
 
-    time.sleep(3)
     try:
         current_page = pywikibot.Page(
             site, u"File:" + json_images[fotonr_plus_ext])
@@ -64,17 +62,6 @@ for index, fotonr in enumerate(old_json.keys()):
         commons_infotext = current_page.latest_revision.text
 
         current_page.text = json_infotext
-
-        ### In production ####
-        # current_page.save(u"Updated infotext from ./mexiko_info_data.json")
-
-        ### Dry-run to check quality ###
-        log.write("old_infotext:\n{}\n\n-------- {} ---------\
-            \naltered_page:\n{}\n\n".format(
-            commons_infotext,
-            json_images[fotonr_plus_ext],
-            json_infotext)
-        )
 
     except Exception as e:
             print(
@@ -86,4 +73,21 @@ for index, fotonr in enumerate(old_json.keys()):
         print("Error on commons file {} source field retrieval:\n{}".format(
             current_page, e))
         # print("current_source:\n{}".format(current_source))
+
+    if commons_infotext != json_infotext:
+        no_alterations += 1
+         # Dry-run to check quality ########
+        log.write("old_infotext:\n{}\n\n-------- {} ---------\
+            \naltered_page:\n{}\n\n".format(
+            commons_infotext,
+            json_images[fotonr_plus_ext],
+            json_infotext)
+        )
+
+        # In production ###################
+        # current_page.save(u"Updated infotext from ./mexiko_info_data.json")
+    else:
+        pass
+
+    print("File no {} of {}. Altered: {}".format(index, len(old_json.keys()), no_alterations))
 log.close()
