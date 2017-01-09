db2f366fcc4bb2f3d0c8604a93138d75ed7422ef
mattiasostmar <mattiasostmar@gmail.com>
2017-01-09 10:36:10 +0100

    rewrite from archive card links to accession number

--- correct_accession_numbers_mexiko.py
+++ correct_accession_numbers_mexiko.py
@@ -4,8 +4,6 @@ import json
 import pywikibot
 import time
 
-alteration_log = open("correct_accession_numbers.log", "w")
-
 site = pywikibot.Site(fam="commons")
 
 fname_map = {}
@@ -51,73 +49,42 @@ mexiko_arkiv_converters = {
 # All column names are strip:ed
 mexiko_arkiv = pd.read_excel("excel-export.xls", sheetname="Mexiko-Arkiv")
 
-has_archive_cards = []
-has_archive_cards.extend(mexiko_arkiv["Fotonummer.1"].dropna().tolist())
-has_archive_cards.extend(mexiko_arkiv["Fotonummer.2"].dropna().tolist())
-has_archive_cards.extend(mexiko_arkiv["Fotonummer.3"].dropna().tolist())
-has_archive_cards.extend(mexiko_arkiv["Fotonummer.4"].dropna().tolist())
-has_archive_cards = [id_str.strip() for id_str in has_archive_cards]  # Note: every id has a leading whitespace in metadat doc!
-
-# print(has_archive_cards)
-
-def create_archive_card_smvk_em_link(fotonr, mexiko_arkiv):
-    arkiv_id_row = mexiko_arkiv[mexiko_arkiv["Fotonummer"] == row["Fotonummer"]]
-    arkiv_id = arkiv_id_row["Arkiv-id"]
-    ac_url = arkiv_id_row["Länk"]
-    left_side, slash, id_str = url.rpartition("/")
-    ac_template = "{{SMVK-EM-link|1=?|2=" + id_str + "|3=" + arkiv_id
-    return ac_template
-
-ac_cols = ["Fotonummer","Fotonummer.1","Fotonummer.2","Fotonummer.3","Fotonummer.4","Fotonummer.5"]
-
 for index, fotonr in enumerate(old_json.keys()):
+    fotonr_plus_ext = fotonr + ".tif"
+
     print("File no {} of {}".format(index, len(old_json.keys())))
-    if fotonr in has_archive_cards:
-        #print("{} has archive card".format(fotonr))
-        
-        for col in ac_cols:
-            #print(mexiko_arkiv[col])
-            if fotonr in mexiko_arkiv[col].str.strip().dropna().tolist():
-                ugly_fotonr = " " + fotonr # due to leading whitespace in original metadata
-                result = mexiko_arkiv[mexiko_arkiv[col] == ugly_fotonr]["Länk"]
-                link_string = result.values[0]
-                #print("{} in in mexiko_arkiv col {}".format(fotonr, col))
-                left, dummy, id_str = link_string.rpartition("/")
-                #print("id_str: {} ".format(id_str))
-                template = "{{SMVK-EM-link|1=arkiv|2=" + id_str + "|3=" + fotonr + "}}"
-                #print("fotonr: {} {}".format(fotonr,template))
-        
-                old_infotext = old_json[fotonr]["info"]
-                source_patt = regex.compile(r"\|source\W+=([ \w,\:<>\/\'\.\n{}|\=]+)permission") # note: catches the initial | of permission fild
-                match = source_patt.search(old_infotext)
-                new_source = "" 
-                new_source += match.group(1).split("\n")[0] + "\n"
-                new_source += match.group(1).split("\n")[1] + "\nRelated archive card: " + template
-                new_source += match.group(1).split("\n")[2] + "\n"
-                #print("old_source:\n{}\nnew_source:\n{}\n ".format(match.group(1), new_source))
-                fotonr_plus_ext = fotonr + ".tif"
-                if fotonr_plus_ext in fname_map:
-                    time.sleep(3)
-                    try:
-                        current_page = pywikibot.Page(site, u"File:" + fname_map[fotonr_plus_ext])
-                        current_infotext = current_page.latest_revision.text # ensure latest revision
-                        current_match = source_patt.search(current_infotext)
-                    except Exception as e:
-                        print("It seems that <fotonummer> {} is not uploaded?\nError message: {}\n".format(fotonr, e))
-                    try:
-                        current_source = current_match.group(1)
-                        altered_infotext = current_infotext.replace(current_source, new_source)
-                        current_page.text = altered_infotext
-                        current_page.save(u"Add link to archive card")
-                        alteration_log.write("Appended archive card link {} to File:{}\n".format(template, fname_map[fotonr_plus_ext]))
+    json_infotext = old_json[fotonr]["info"]
 
-                        #print("old_infotext:\n{}\n\n-------- {} ---------\naltered_page:\n{}\n\n".format(current_infotext, fname_map[fotonr_plus_ext], altered_infotext))
-                    except AttributeError as e:
-                        print("Error on commons file {} source field retrieval:\n{}".format(current_page, e))
-                    #print("current_source:\n{}".format(current_source))
-                else:
-                    print("{} not in fname_map, thus wasn't good enough to be uploaded".format(fotonr))
-alteration_log.close()
+    time.sleep(3)
+    try:
+        current_page = pywikibot.Page(
+            site, u"File:" + fname_map[fotonr_plus_ext])
+        # ensure latest revision
+        commons_infotext = current_page.latest_revision.text
+    except Exception as e:
+            print(
+                "It seems <fotonummer> {} is not uploaded? \
+                \nError: {}\n".format(
+                    fotonr, e))
 
+    print(
+        "old_source:\n{}\nnew_source:\n{}\n ".format(
+            json_infotext, commons_infotext))
 
-re.match("hej","jag är hej som")
+    try:
+        current_page.text = json_infotext
+        # current_page.save(u"Updated infotext from ./mexiko_info_data.json")
+
+        print("old_infotext:\n{}\n\n-------- {} ---------\
+            \naltered_page:\n{}\n\n".format(
+            commons_infotext,
+            fname_map[fotonr_plus_ext],
+            json_infotext)
+        )
+    except AttributeError as e:
+        print("Error on commons file {} source field retrieval:\n{}".format(
+            current_page, e))
+        # print("current_source:\n{}".format(current_source))
+    else:
+        print("{} not in fname_map, thus wasn't good enough \
+            to be uploaded".format(fotonr))
