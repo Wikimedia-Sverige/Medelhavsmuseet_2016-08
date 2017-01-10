9d5565c802960aa488b2f1e0b9f46d26d716e2f1
Mattias Östmar <mattiasostmar@gmail.com>
2017-01-10 22:21:33 +0100

    add deletation template and printout result

--- check_pages_without_images_smvk-em.py
+++ check_pages_without_images_smvk-em.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import re
+
 import pywikibot
 from pywikibot import pagegenerators as pg
-import re
 
 site = pywikibot.Site(fam="commons")
 cat = pywikibot.Category(site, 'Category:Media_from_the_National_Museums_of_World_Culture')
@@ -18,13 +19,28 @@ fname_patt = re.compile(r'\[\[commons\:(File:[\w \_\-\.\,\(\)]+)\]\]')
 
 bad_images = []
 
+
+def add_deletion_template(page):
+    """
+    Fetch text from pywikibot filePage (since it's on Commons) object
+    and add a template for speedy deletation to top, save it and
+    return success message or error message.
+
+    :rtype: string
+    """
+    current_infotext = page.latest_revision.text
+    new_infotext = "{{speedydelete|broken file upload}}\n" + current_infotext
+    print(new_infotext)
+
 for page in gen:
     filePage = pywikibot.FilePage(page)
     try:
         print(filePage.fileIsShared())
     except pywikibot.exceptions.PageRelatedError as e:
         match = fname_patt.search(str(e))
-        print(match.group(1))
+        # print(match.group(1))
+        add_deletion_template(page)
+
         bad_images.append(match.group(1))
 
 print(len(bad_images))
